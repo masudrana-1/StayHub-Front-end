5f0e25375176b52fb63f73caf840427a6a6c5544
masudrana-1 <masudrana58855@gmail.com>
2024-01-04 11:05:39 +0600

    add container padding in tailwind css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom"
 const Header = () => {
     return (
         <div className='bg-blue-800 py-6'>
-            <div className='container mx-auto px-4 flex justify-between'>
+            <div className='container mx-auto flex justify-between'>
                 <span className='text-3xl text-white font-bold tracking-tight'>
                     <Link to="/">ZenStayHub.com</Link>
                 </span>
--- src/components/HeaderText.tsx
+++ src/components/HeaderText.tsx
@@ -3,7 +3,7 @@
 const HeaderText = () => {
     return (
         <div className="bg-blue-800 pb-16">
-            <div className="container mx-auto px-4 flex flex-col gap-2">
+            <div className="container mx-auto flex flex-col gap-2">
                 <h1 className="text-5xl text-white font-bold">
                     Find your next stay
                 </h1>
