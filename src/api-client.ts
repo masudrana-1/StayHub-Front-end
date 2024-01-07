b857fae15e535e57446183b33f75d2c3a2af17b2
masudrana-1 <masudrana58855@gmail.com>
2024-01-07 18:42:44 +0600

    fetch api for post

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,5 +1,9 @@
 import { RegisterFormData } from "./pages/Register";
 
+// env 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// fetch register api 
 export const register =async (formData:RegisterFormData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
         method: "POST",
@@ -7,5 +11,11 @@ export const register =async (formData:RegisterFormData) => {
             "Content-type": "application/json"
         },
         body: JSON.stringify(formData),
-    })
+    });
+
+    const responseBody = await response.json();
+
+    if (!response.ok) {
+        throw new Error(responseBody.message);
+    }
 }
