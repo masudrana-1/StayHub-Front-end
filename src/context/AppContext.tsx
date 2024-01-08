0e20805dc339a7da5bb89c64cc49aeb5e871c1ca
masudrana-1 <masudrana58855@gmail.com>
2024-01-08 10:49:26 +0600

    show toast message

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // toast 
 type ToastMessage = {
@@ -16,8 +16,16 @@ const AppContext = React.createContext<AppContext | undefined>(undefined);
 
 export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
     return (
-        <AppContext.Provider value={{showToast: ()=> undefined}}>
+        <AppContext.Provider value={{
+            showToast: (toastMessage) =>
+                console.log(toastMessage)
+        }}>
             {children}
         </AppContext.Provider>
-    )
-}
+    );
+};
+
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+    return context as AppContext;
+};
