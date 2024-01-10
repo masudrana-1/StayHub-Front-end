474cc209b3e261acd201f97aa971c99d728be03c
masudrana-1 <masudrana58855@gmail.com>
2024-01-10 10:59:32 +0600

    create email and password input

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 
 export type SignInFormData = {
@@ -9,12 +10,54 @@ export type SignInFormData = {
 
 const SignIn = () => {
 
-    const {register}= useForm()
+    const {register}= useForm<SignInFormData>()
 
     return (
-        <from>
-            
-        </from>
+        <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+            <h2 className="text-3xl font-bold">Sign In</h2>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Email
+                <input
+                    type="email"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("email", { required: "This field is required" })}
+                ></input>
+                {errors.email && (
+                <span className="text-red-500">{errors.email.message}</span>
+                )}
+            </label>
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Password
+                <input
+                    type="password"
+                    className="border rounded w-full py-1 px-2 font-normal"
+                    {...register("password", {
+                        required: "This field is required",
+                        minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters",
+                    },
+                })}
+                ></input>
+                {errors.password && (
+                    <span className="text-red-500">{errors.password.message}</span>
+                )}
+            </label>
+            <span className="flex items-center justify-between">
+                <span className="text-sm">
+                    Not Registered?{" "}
+                    <Link className="underline" to="/register">
+                        Create an account here
+                    </Link>
+                </span>
+                <button
+                    type="submit"
+                    className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+                >
+                    Login
+                </button>
+            </span>
+        </form>
     );
 };
 
