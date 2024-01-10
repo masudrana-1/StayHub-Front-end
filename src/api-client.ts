d893e580bd868b0fd3c434fdbfbf3a9dde544ffe
masudrana-1 <masudrana58855@gmail.com>
2024-01-10 11:30:37 +0600

    fetch sign in data and send response

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,4 +1,5 @@
 import { RegisterFormData } from "./pages/Register";
+import { SignInFormData } from "./pages/SignIn";
 
 // env 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -22,6 +23,24 @@ export const register =async (formData:RegisterFormData) => {
         throw new Error(responseBody.message);
     }
 }
+
+// sign in data fetch 
+export const signIn = async (formData: SignInFormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
+
+    const body = await response.json();
+    if (!response.ok) {
+        throw new Error(body.message);
+    }
+    return body;
+};
 
 // get token 
 export const validateToken = async () => {
